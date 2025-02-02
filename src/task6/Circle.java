1d3b1d26113674d7fa4cedf034f50198e7b13834
nadyazhigarevich <zhigarevichnadya@gmail.com>
2025-02-02 10:01:55 +0300

    first commit

---
+++ src/task6/Circle.java
@@ -0,0 +1,8 @@
+package task6;
+
+public class Circle extends Shape{
+    @Override
+    public void draw() {
+        System.out.println("The circle is being drawn");
+    }
+}
---
+++ src/task6/Rectangle.java
@@ -0,0 +1,8 @@
+package task6;
+
+public class Rectangle extends Shape{
+    @Override
+    public void draw() {
+        System.out.println("The rectangle is being drawn");
+    }
+}
