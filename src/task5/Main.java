35bb1be3320d7ed530d3ca0f1f018a000514a4a7
nadyazhigarevich <zhigarevichnadya@gmail.com>
2025-02-02 09:47:35 +0300

    Add: task5

---
+++ src/task5/Main.java
@@ -0,0 +1,11 @@
+package task5;
+
+public class Main {
+    public static void main(String[] args) {
+        Piano piano = new Piano();
+        Guitar guitar = new Guitar();
+
+        piano.play();
+        guitar.play();
+    }
+}
---
+++ src/task5/Playable.java
@@ -0,0 +1,5 @@
+package task5;
+
+public interface Playable {
+    public void play();
+}
